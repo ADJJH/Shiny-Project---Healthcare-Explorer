f41e1d13baa201ded2b26642e72b1228cbde2abe
Ale <jana.yeims@gmail.com>
2015-07-01 16:40:42 -0400

    Changed path of sources

--- Loading_data.R
+++ Loading_data.R
@@ -2,9 +2,9 @@ library(dplyr)
 library(xlsx)
 
 #Importing data 
-docs_consultation = read.csv("Shiny\ Project/Sources/DOCCONSULT_60_13.csv",
+docs_consultation = read.csv("Shiny Project/Prohect-Shiny/Sources/DOCCONSULT_60_13.csv",
                             skip = 8, header=TRUE, check.names=FALSE )[ ,1:55]
 
 
-remuneration = read.xlsx2("Shiny Project/Sources/Remunerations of doctors ratio_2011.xls",
+remuneration = read.xlsx2("Shiny Project/Prohect-Shiny/Sources//Remunerations of doctors ratio_2011.xls",
                           sheetIndex="Data3.6.1",startRow=6 ,endRow=29)
