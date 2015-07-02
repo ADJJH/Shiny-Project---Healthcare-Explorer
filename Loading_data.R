712b40254b2268f9e2a919ba5ebcbbd8e575fec6
Ale <jana.yeims@gmail.com>
2015-07-01 23:01:40 -0400

    Consultation cleaning and saving files in rds

--- Loading_data.R
+++ Loading_data.R
@@ -16,6 +16,8 @@ colnames(remuneration) = c("Location","Year","GP_Salaried",
                            "GP_Self","Specialist_Salaried",
                            "Specialist_Self")
 
+## DF REMUNERATION
+
 # Years can be different or empty - documentaiton says 2011 - so that year is taken as basis
 remuneration$Year = 2011
 
@@ -24,6 +26,19 @@ remuneration$Location = str_replace_all(remuneration$Location, "[^A-z]", "")
 
 # Tranposing columns GP, specialist, self employed and salaried
 remuneration <- melt(remuneration, id=c("Location","Year"))
+colnames(remuneration) = c("Location","Year","Type_doc_job",'Salary')
+
+## DF DOCS_CONSULTATION
+
+#Transposing years
+docs_consultation1 = melt(docs_consultation,id = 'Location')
+colnames(docs_consultation) = c("Location","Year", "Nmr_consult")
+
+
+###### Saving dfs
+saveRDS(docs_consultation, file="Shiny Project/Prohect-Shiny/data/docs_consultation.rds")
+saveRDS(remuneration, file="Shiny Project/Prohect-Shiny/data/remuneration.rds")
+
 
 
 
