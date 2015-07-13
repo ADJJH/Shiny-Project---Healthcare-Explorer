3aa85bdde3d55740894ac8b59129428f0787fa73
Ale <jana.yeims@gmail.com>
2015-07-13 19:32:39 -0400

    removing extra files

--- Loading_data.R
+++ Loading_data.R
@@ -3,6 +3,46 @@ library(xlsx)
 library(reshape)
 library (stringr)
 
+countries_oecd = c("",
+                   'Australia',
+                   'Austria',
+                   'Belgium',
+                   'Brazil',
+                   'Canada',
+                   'Chile',
+                   'Czech Republic',
+                   'Denmark',
+                   'Estonia',
+                   'Finland',
+                   'France',
+                   'Germany',
+                   'Greece',
+                   'Hungary',
+                   'Iceland',
+                   'Ireland',
+                   'Israel',
+                   'Italy',
+                   'Japan',
+                   'Korea',
+                   'Luxembourg',
+                   'Mexico',
+                   'Netherlands',
+                   'New Zealand',
+                   'Norway',
+                   'Poland',
+                   'Portugal',
+                   'Russia',
+                   'Slovak Republic',
+                   'Slovenia',
+                   'South Africa',
+                   'Spain',
+                   'Switzerland',
+                   'Turkey',
+                   'United Kingdom',
+                   'United States'
+)
+
+
 #************************************ Upper left ************************************************
 
 ##### Importing data 
@@ -24,7 +64,7 @@ colnames(remuneration) = c("Location","Year","GP_Salaried",
 remuneration$Year = 2011
 
 #Removing non alpha strings
-remuneration$Location = str_replace_all(remuneration$Location, "[^A-z]", "")
+#remuneration$Location = str_replace_all(remuneration$Location, "[^A-z]", "")
 
 # Tranposing columns GP, specialist, self employed and salaried
 remuneration <- melt(remuneration, id=c("Location","Year"))
@@ -46,14 +86,141 @@ saveRDS(remuneration, file="Shiny Project/Prohect-Shiny/data/remuneration.rds")
 beds = read.csv("Shiny Project/Prohect-Shiny/Sources/HOSPITALBED-ACUTE-1000HAB_60-13.csv",
                              skip = 8, header=TRUE, check.names=FALSE )[ ,1:55]
 
+beds = melt(beds,id = 'Location')
+colnames(beds) = c("Location","Year", "beds_nmr")
+
+# only up to 2012!
+
+hosp_stay = read.csv("Shiny Project/Prohect-Shiny/Sources/HOSPITALSTAY_60_13.csv",
+                       skip = 8, header=TRUE, check.names=FALSE )[ ,1:54]
+hosp_stay = melt(hosp_stay,id = 'Location')
+colnames(hosp_stay) = c("Location","Year", "nmr_days")
+
+
+saveRDS(beds, file="Shiny Project/Prohect-Shiny/data/beds.rds")
+saveRDS(hosp_stay, file="Shiny Project/Prohect-Shiny/data/hosp_stay.rds")
+
+
+
+#********************************** bottom_right **********************************
+
+##### Importing data 
+population = read.csv("Shiny Project/Prohect-Shiny/Sources/POP-TOT-MLN_PER_1960_2014.csv",
+                skip = 8, header=TRUE, check.names=FALSE )[ ,1:55]
+
+population = melt(population,id = 'Location')
+colnames(population) = c("Location","Year", "habitants")
+population = filter(population,Location  %in%  countries_oecd)
+
+life_exp = read.csv("Shiny Project/Prohect-Shiny/Sources/LIFEEXP-TOT-1960-2013.csv",
+                     skip = 8, header=TRUE, check.names=FALSE )[ ,1:55]
+life_exp = melt(life_exp,id = 'Location')
+colnames(life_exp) = c("Location","Year", "exp_years")
+life_exp = filter(life_exp,Location  %in%  countries_oecd)
 
 
 
 
+saveRDS(life_exp, file="Shiny Project/Prohect-Shiny/data/life_exp.rds")
+saveRDS(population, file="Shiny Project/Prohect-Shiny/data/population.rds")
+
+
+#************** bottom left **************************************************
+spending = read.csv("Shiny Project/Prohect-Shiny/Sources/HEALTHEXP-TOT-PC_GDP-60_13.csv",
+                      skip = 8, header=TRUE, check.names=FALSE )[ ,1:55]
+
+spending = melt(spending,id = 'Location')
+colnames(spending) = c("Location","Year", "GDPp")
+
+
+mri = read.csv("Shiny Project/Prohect-Shiny/Sources/MRIUNITS-TOT-82_13.csv",
+                    skip = 8, header=TRUE, check.names=FALSE )[ ,1:33]
+mri = melt(mri,id = 'Location')
+colnames(mri) = c("Location","Year", "med_units")
+
+ct = read.csv("Shiny Project/Prohect-Shiny/Sources/CTSCANNER-TOT-1000000HAB_80-13.csv",
+               skip = 8, header=TRUE, check.names=FALSE )[ ,1:35]
+ct = melt(ct,id = 'Location')
+colnames(ct) = c("Location","Year", "med_units")
+
+saveRDS(spending, file="Shiny Project/Prohect-Shiny/data/spending.rds")
+saveRDS(mri, file="Shiny Project/Prohect-Shiny/data/mri.rds")
+saveRDS(ct, file="Shiny Project/Prohect-Shiny/data/ct.rds")
+
+####################################################################
+#******************** Second tab **********************************
+
+#************* doctors and nurses totals
+
+nurses = read.csv("Shiny Project/Prohect-Shiny/Sources/NURSE-TOT-1000HAB_60_13.csv",
+                    skip = 8, header=TRUE, check.names=FALSE ,stringsAsFactors=FALSE)[ ,1:36]
+nurses = melt(nurses,id = 'Location')
+colnames(nurses) = c("Location","Year", "professionals")
+nurses = mutate(nurses,type="Nurse")
+
+
+docs = read.csv("Shiny Project/Prohect-Shiny/Sources/MEDICALDOC-TOT-1000HAB_60_13.csv",
+                  skip = 8, header=TRUE, check.names=FALSE,stringsAsFactors=FALSE )[ ,1:55]
+docs = melt(docs,id = 'Location')
+colnames(docs) = c("Location","Year", "professionals")
+docs = mutate(docs,type="Doctor")
+
+
+professionals= rbind(nurses,docs)
+saveRDS(professionals, file="Shiny Project/Prohect-Shiny/data/professionals.rds")
+
+#************* doctors and nurses graduates
+
+nurses = read.csv("Shiny Project/Prohect-Shiny/Sources/NURSEGRAD-TOT-100000HAB_80_13.csv",
+                  skip = 8, header=TRUE, check.names=FALSE ,stringsAsFactors=FALSE)[ ,1:35]
+nurses = melt(nurses,id = 'Location')
+colnames(nurses) = c("Location","Year", "graduates")
+nurses = mutate(nurses,type="Nurse")
+
+
+docs = read.csv("Shiny Project/Prohect-Shiny/Sources/MEDICALGRAD-TOT-100000HAB_80_13.csv",
+                skip = 8, header=TRUE, check.names=FALSE,stringsAsFactors=FALSE )[ ,1:35]
+docs = melt(docs,id = 'Location')
+colnames(docs) = c("Location","Year", "graduates")
+docs = mutate(docs,type="Doctor")
+
+
+graduates= rbind(nurses,docs)
+saveRDS(graduates, file="Shiny Project/Prohect-Shiny/data/graduates.rds")
+
+
+#*********** mri, ct exams
+
+mri_exams_h = read.csv("Shiny Project/Prohect-Shiny/Sources/MRIEXAM-INHOSPITAL-1000HAB_90_13.csv",
+                       skip = 8, header=TRUE, check.names=FALSE ,stringsAsFactors=FALSE)[ ,1:20]
+mri_exams_h = melt(mri_exams_h,id = 'Location')
+colnames(mri_exams_h) = c("Location","Year", "exams")
+mri_exams_h = mutate(mri_exams_h,type="Hospital")
+
+mri_exams_a = read.csv("Shiny Project/Prohect-Shiny/Sources/MRIEXAM-INAMBULATORY-1000HAB_90_13.csv",
+                       skip = 8, header=TRUE, check.names=FALSE ,stringsAsFactors=FALSE)[ ,1:20]
+mri_exams_a = melt(mri_exams_a,id = 'Location')
+colnames(mri_exams_a) = c("Location","Year", "exams")
+mri_exams_a = mutate(mri_exams_a,type="Ambulatory")
+
+mri_exams = rbind(mri_exams_h,mri_exams_a)
 
 
 
+ct_exams_h = read.csv("Shiny Project/Prohect-Shiny/Sources/CTEXAM-INHOSPITAL-1000HAB_90_13.csv",
+                       skip = 8, header=TRUE, check.names=FALSE ,stringsAsFactors=FALSE)[ ,1:25]
+ct_exams_h = melt(ct_exams_h,id = 'Location')
+colnames(ct_exams_h) = c("Location","Year", "exams")
+ct_exams_h = mutate(ct_exams_h,type="Hospital")
+
+ct_exams_a = read.csv("Shiny Project/Prohect-Shiny/Sources/CTEXAM-INAMBULATORY-1000HAB_90_13.csv",
+                       skip = 8, header=TRUE, check.names=FALSE ,stringsAsFactors=FALSE)[ ,1:20]
+ct_exams_a = melt(ct_exams_a,id = 'Location')
+colnames(ct_exams_a) = c("Location","Year", "exams")
+ct_exams_a = mutate(ct_exams_a,type="Ambulatory")
+
+ct_exams = rbind(ct_exams_h,ct_exams_a)
 
 
-
-
+saveRDS(mri_exams, file="Shiny Project/Prohect-Shiny/data/mri_exams.rds")
+saveRDS(ct_exams, file="Shiny Project/Prohect-Shiny/data/ct_exams.rds")
